f2db9d0e66bde8daadc7e3affdce9cf49043df06
pluxurypinkphloydrockstar <thedianab15@gmail.com>
2023-10-26 12:59:17 +0300

    типо связь с прологом

--- prolog_link.py
+++ prolog_link.py
@@ -4,6 +4,7 @@ file_of_examples = open('./prolog_minesweeper/minesweeper.pl', 'r')
 examples = file_of_examples.read().split('.')
 
 prolog = Prolog()
+
 prolog.consult("./prolog_minesweeper/minesweeper.pl")
 
 
