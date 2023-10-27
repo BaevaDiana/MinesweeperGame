0a9558b4f41e2a5af4ae396be2c2575eb473ff23
Кристина <kristi090217@gmail.com>
2023-10-27 09:21:06 +0300

    интерфейс готов

--- Interface.py
+++ Interface.py
@@ -1,22 +1,20 @@
 import random
 from pyswip import Prolog
 from tkinter import *
+from tkinter import Button
 from  random import choice
 import time
 
-#сделать везде масиив input_filed
-
-
 xBtn=16;yBtn=16;
-frm = []; btn = []  # Списки с фреймами и кнопками
+btn = [[]]  # Списки с кнопками
 playTime = 0  # Время игры
 imgMark = '\u2661'; imgMine = '\u2665'  # Символ маркера и мины
-nMoves = 0; mrk = 0  # Игровое поле, счётчик ходов и маркеров
+nMoves = 0; mrk = 0  #Счётчик ходов и маркеров
 tk = Tk()
 tk.title('Игра Сапер')
 tk.geometry(str(44 * xBtn) + 'x' + str(44 * yBtn + 10))
 
-def play(n):  # Обработка нажатия на клетку
+def play(ni,nj):  # Обработка нажатия на клетку
     global xBtn, yBtn,input_field, mines, nMoves, mrk, playTime
     if len(input_field) < xBtn * yBtn:  # Если поле ещё не создано
         return()
@@ -36,56 +34,62 @@ def play(n):  # Обработка нажатия на клетку
                     if input_field[i][j+1] == "*": k += 1  # сверху
                     if input_field[i][j-1] == '*': k += 1  # снизу
                     input_field[i][j] = k
-    if btn[n].cget('text') == imgMark:  # Если поле было промаркировано
+    if btn[ni][nj].cget('text') == imgMark:  # Если поле было промаркировано
         mrk -= 1
         tk.title('Осторожно! Вокруг ' + str(mines - mrk) + ' Мин(ы)!!!!')
-    btn[n].config(text=input_field[n], state=DISABLED, bg='white')  # Отображаем игровую ситуацию
-    if input_field[n][n] == 0:  # Пустое поле без соседей
-        btn[n].config(text=' ', bg='#ccb')
-    elif input_field[n][n] == '*':  # Мина
-        btn[n].config(text=imgMine)
-        if nMoves <= (xBtn * yBtn - mines) or mines >= mrk:  # Если игрок ещё не выиграл, то проиграл
+    btn[ni][nj].config(text=input_field[ni][nj], state=DISABLED, bg='white')  # Отображаем игровую ситуацию
+    if input_field[ni][nj] == 0:  # Пустое поле без соседей
+        btn[ni][nj].config(text=' ', bg='#ccb')
+    elif input_field[ni][nj] == '*':  # Мина
+        btn[ni][nj].config(text=imgMine)
+        if nMoves <= (xBtn * yBtn - mines) or mines >= mrk:  # Если открытых клеток меньше чем может быть или мин больше помеченных,то проиграл
             nMoves = 32000  # Если проиграл, то уже не выиграет
-            chainReaction(0)  # Цепная реакция
+            chainReaction(0,0)  # Цепная реакция
             tk.title('Вы проиграли :(')
     if nMoves == (xBtn * yBtn - mines) and mines == mrk:  # Если все клетки открыты и мины помечены
         tk.title('Поздравляю! Вы выиграли за ' + str(int(time.time() - playTime)) + ' сек')
-        winner(0)
+        winner(0,0)
 #
-def chainReaction(j):  # Цепная реакция
-    if j <= len(input_field):  # Если не запустили новую игру
-        for i in range(j, xBtn * yBtn):
-            if input_field[i] == -1 and btn[i].cget('text') == ' ':
-                btn[i].config(text=imgMine)
-                btn[i].flash()
+def chainReaction(i,j):  # Цепная реакция при проигрыше
+    if i < xBtn:
+        if j < yBtn:
+            if input_field[i][j] == -1 and btn[i][j].cget('text') == ' ': #мина и клетка пустая
+                btn[i][j].config(text=imgMine)
+                btn[i][j].flash()
                 tk.bell()
-                tk.after(50, chainReaction, i + 1)
-                break
-
-def winner(j):  # Победа
-    if j <= len(input_field):  # Если не запустили новую игру
-        for i in range(j, xBtn * yBtn):
-            if input_field[i] == 0:
-                btn[i].config(state=NORMAL, text='☺')
-                btn[i].flash()
-                tk.bell()
-                btn[i].config(text=' ', state=DISABLED)
-                tk.after(50, winner, i + 1)
-                break
-
-def marker(n):  # Помечаем, где возможно скрывается мина
-    global mrk, mines, playTime
-    if (btn[n].cget('state')) != 'disabled':
-        if btn[n].cget('text') == imgMark:
-            btn[n].config(text=' ')
-            mrk -= 1
+                tk.after(50, chainReaction, i, j + 1)  # Установить задержку 50 миллисекунд и рекурсивно вызвать функцию
         else:
-            btn[n].config(text=imgMark, fg='blue')
-            mrk += 1
-        tk.title('Осторожно! Вокруг ' + str(mines - mrk) + ' Мин!!!!')
-    if nMoves == (xBtn * yBtn - mines) and mines == mrk:  # Если все клетки открыты и мины помечены
-        tk.title('Поздравляю! Вы выиграли за ' + str(int(time.time() - playTime)) + ' сек')
-        winner(0)
+            chainReaction(i + 1, 0)  # Если j превышает yBtn, вызвать для следующего i и сбросить j в 0
+
+
+def winner(i, j):  # Функция для отображения победы
+    if i < xBtn:
+        if j < yBtn:
+            if input_field[i][j] == 0:  # Если значение в массиве input_field равно 0
+                btn[i][j].config(state=NORMAL, text='☺')  # Настроить кнопку btn на отображение "☺"
+                btn[i][j].flash()  # Анимировать кнопку
+                tk.bell()  # Воспроизвести звуковой сигнал
+                btn[i][j].config(text=' ', state=DISABLED)  # Снова настроить кнопку на отображение пустоты и отключение
+                tk.after(50, winner, i, j + 1)  # Установить задержку 50 миллисекунд и рекурсивно вызвать функцию winner для следующего j
+        else:
+            winner(i + 1, 0)  # Если j превышает yBtn, вызвать winner для следующего i и сбросить j в 0
+
+
+def marker(ni, nj):  # Функция для пометки клеток, где могут быть мины
+    global mrk, mines, playTime
+    if (btn[ni][nj].cget('state')) != 'disabled':  # Проверка, что кнопка не отключена
+        if btn[ni][nj].cget('text') == imgMark:  # Если на кнопке уже установлена метка
+            btn[ni][nj].config(text=' ')  # Убрать метку
+            mrk -= 1  # Уменьшить количество помеченных мин
+        else:
+            btn[ni][nj].config(text=imgMark, fg='blue')  # Установить метку на кнопке с синим текстом
+            mrk += 1  # Увеличить количество помеченных мин
+        tk.title('Осторожно! Вокруг ' + str(mines - mrk) + ' Мин!!!!')  # Обновление заголовка окна
+
+    if nMoves == (xBtn * yBtn - mines) and mines == mrk:  # Если все клетки открыты и все мины помечены
+        tk.title('Поздравляю! Вы выиграли за ' + str(int(time.time() - playTime)) + ' сек')  # Отобразить сообщение о победе
+        winner(0, 0)  # Вызвать функцию winner для завершения игры и отображения анимации победы
+
 
 
 def newGame():  # Начать новую игру
@@ -94,12 +98,10 @@ def newGame():  # Начать новую игру
     mrk = 0
     input_field.clear()
     if len(btn) != 0:
-        for i in range(0, len(btn)):
-            btn[i].destroy()
+        for i in range(0, xBtn):
+            for j in range(0, yBtn):
+                btn[i][j].destroy()
         btn.clear()
-        for i in range(0, len(frm)):
-            frm[i].destroy()
-        frm.clear()
     playground()
     tk.title('Осторожно! Вокруг ' + str(mines - mrk) + ' Мин(ы)!!!!')
 
@@ -165,20 +167,28 @@ def set16x16():  # Установить размер поля 16x16
 
 def playground():  # Создать игровое поле
     global xBtn, yBtn
-    for i in range(0, yBtn):
-        frm.append(Frame())
-        frm[i].pack(expand=YES, fill=BOTH)
-        for j in range(0, xBtn):
-            btn.append(Button(frm[i], text=' ', font=('mono', 16, 'bold'),
-                              width=1, height=1, padx=0, pady=0))
-    for i in range(0, xBtn * yBtn):
-        if xBtn * yBtn > 256:
-            btn[i].config(font=('mono', 8, 'normal'))
-        btn[i].config(command=lambda n=i: play(n))
-        btn[i].bind('<Button-3>', lambda event, n=i: marker(n))
-        btn[i].pack(side=LEFT, expand=YES, fill=BOTH, padx=0, pady=0)
-        btn[i].update()
 
+    # Создание и размещение кнопок на графическом интерфейсе
+    for i in range(yBtn):
+        for j in range(xBtn):
+            btn[i][j] = tk.Button(tk, text=' ', font=('mono', 16, 'bold'), width=1, height=1, padx=0, pady=0)
+            btn[i][j].grid(row=i, column=j)
+
+    # Изменение размера шрифта, если общее количество кнопок больше 256
+    for i in range(0, xBtn):
+        for j in range(0, yBtn):
+            if xBtn * yBtn > 256:
+                btn[i][j].config(font=('mono', 8, 'normal'))
+
+            # Назначение функции play(i, j) на кнопку для обработки кликов
+            btn[i][j].config(command=play(i, j))
+
+            # Привязка функции marker(n, j) к правой кнопке мыши (Button-3)
+            btn[i][j].bind('<Button-3>', lambda event, n=i: marker(n, j))
+
+            # Настройка параметров и отображение кнопки
+            btn[i][j].pack(side=LEFT, expand=YES, fill=BOTH, padx=0, pady=0)
+            btn[i][j].update()
 
 
 frmTop = Frame()  # Создаем верхний фрейм для кнопок "New game"
